c520f3a2de73c03414309067e9f4a1ebf9c5b151
Robinhfelektro <100714494+Robinhfelektro@users.noreply.github.com>
2022-03-23 22:52:52 +0100

    added eventgroup

--- U_PiCalc/main.c
+++ U_PiCalc/main.c
@@ -34,6 +34,16 @@ void controllerTask(void* pvParameters);
 void leibnizTask(void* pvParameters);
 
 
+
+
+
+
+#define egPI_CALCULATE_PROGRESS		1	<< 0
+#define egPI_CALCULATE_DONE			1	<< 1
+#define egPI_WRITE_DISPLAY			1	<< 2
+
+EventGroupHandle_t egPI_Calc;
+
 float pi4 = 1;
 float pi = 0;  
 
@@ -41,6 +51,8 @@ int main(void)
 {
     vInitClock();
 	vInitDisplay();
+	
+	egPI_Calc = xEventGroupCreate();
 	
 	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL);
 	xTaskCreate( leibnizTask, (const char *) "leibniz_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
@@ -57,11 +69,20 @@ void leibnizTask(void* pvParameters) {
 	
 	while(1)
 	{
+		xEventGroupSetBits(egPI_Calc, egPI_CALCULATE_PROGRESS);		//calculate Flag
+		
+		xEventGroupWaitBits(egPI_Calc, egPI_WRITE_DISPLAY, pdTRUE, pdFALSE, portMAX_DELAY);  //wait for calculate enable?
+		
 		pi4 = pi4 - (1.0 / counter);  //1.0 damit als float erkannt int counter
 		counter += 2; 
 		pi4 = pi4 + (1. / counter);
 		counter += 2; 
 		pi = pi4 * 4; 
+		
+		if (pi >= 3.14)
+		{
+			xEventGroupSetBits(egPI_Calc, egPI_CALCULATE_DONE);		//pi calculated
+		}
 	}
 	
 }
@@ -77,12 +98,15 @@ void controllerTask(void* pvParameters) {
 		
 		if (counter_500ms == 0)        //ausgabe alle 500 ms, 50 * 10ms
 		{
+			xEventGroupWaitBits(egPI_Calc, egPI_CALCULATE_DONE, pdTRUE, pdFALSE, portMAX_DELAY);  //wait for finished pi calculation
+			xEventGroupClearBits(egPI_Calc, egPI_WRITE_DISPLAY);   //block calculating pi?
 			vDisplayClear();
 			counter_500ms = 50; 
 			char pistring[12];
 			//sprintf(&pistring[0], "PI: %.8f", M_PI);
 			sprintf(&pistring[0], "PI: %.8f", pi);
 			vDisplayWriteStringAtPos(1,0, "%s", pistring);
+			xEventGroupSetBits(egPI_Calc, egPI_WRITE_DISPLAY);			//enable calculating pi?
 		}
 		else
 		{
