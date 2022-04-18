b933ae7e5c151fe4bc6508d06e8bb59314d313a7
Robinhfelektro <100714494+Robinhfelektro@users.noreply.github.com>
2022-04-18 13:25:24 +0200

    added comments

--- U_PiCalc/main.c
+++ U_PiCalc/main.c
@@ -2,7 +2,7 @@
  * U_PiCalc_FS2022
  *
  * Created: 20.03.2018 18:32:07
- * Author : Robin bühler
+ * Author : Robin Bühler
  */ 
 
 #include <math.h>
@@ -32,15 +32,17 @@
 #include "avr_f64.h"												//Library Include
 
 
-void vControllerTask(void* pvParameters);
-void vLeibnizTask(void* pvParameters);
-void vEuler_PI(void* pvParameters);
+
+//Switch
+#define STATE_STOP 0
+#define STATE_START 1
 
 
 
-
+//////////////////////////////////////////////////////////////////////////
+// Eventgroup and defines
+//////////////////////////////////////////////////////////////////////////
 EventGroupHandle_t egPI_Calc;
-
 #define	TASK_CHUDNOVSKY				1 << 0	
 #define TASK_LEIBNITZ				1 << 1		
 #define TASK_EULER					1 << 2		 
@@ -62,34 +64,21 @@ EventGroupHandle_t egPI_Calc;
 #define RESET_EG_BUTTONS			0x780 //Reset all Buttons
 
 
-//Switch
-#define STATE_STOP 0
-#define STATE_START 1
-
-
+//////////////////////////////////////////////////////////////////////////
+// Globale Variabeln, für die PI Berechnung
+//////////////////////////////////////////////////////////////////////////
 float32_t Leib_PI = 0;  
 float32_t g_Euler_PI = 0; 
-float64_t g_f_Euler_PI;
-
-int main(void)
-{
-    vInitClock();
-	vInitDisplay();
-	
-	egPI_Calc = xEventGroupCreate();
-
-	
-	xTaskCreate( vControllerTask,	(const char *) "control_tsk",	configMINIMAL_STACK_SIZE+1000, NULL, 1, NULL);
-	xTaskCreate( vLeibnizTask,		(const char *) "leibniz_tsk",	configMINIMAL_STACK_SIZE+500, NULL, 3, NULL);
-	xTaskCreate( vEuler_PI,			(const char *) "Euler_PI_tsk",	configMINIMAL_STACK_SIZE+500, NULL, 3, NULL);
-	
-	vDisplayClear();
-	vDisplayWriteStringAtPos(0,0,"PI-Calc FS2022");
-	vTaskStartScheduler();
-	return 0;
-}
+float64_t g_f_Euler_PI;				
 
 
+void vControllerTask(void* pvParameters);
+void vLeibnizTask(void* pvParameters);
+void vEuler_PI(void* pvParameters);
+
+//////////////////////////////////////////////////////////////////////////
+// Funktion für Fakultät Berechnung
+//////////////////////////////////////////////////////////////////////////
 uint64_t rCalcFakultaet(uint64_t n)
 {
 	int c;
@@ -101,55 +90,66 @@ uint64_t rCalcFakultaet(uint64_t n)
 	return r;
 }
 
+//////////////////////////////////////////////////////////////////////////
+// Initialisierung
+//////////////////////////////////////////////////////////////////////////
+int main(void)
+{
+    vInitClock();
+	vInitDisplay();
+	
+	egPI_Calc = xEventGroupCreate();
+
+	xTaskCreate( vControllerTask,	(const char *) "control_tsk",	configMINIMAL_STACK_SIZE+1000, NULL, 1, NULL);
+	xTaskCreate( vLeibnizTask,		(const char *) "leibniz_tsk",	configMINIMAL_STACK_SIZE+500, NULL, 3, NULL);
+	xTaskCreate( vEuler_PI,			(const char *) "Euler_PI_tsk",	configMINIMAL_STACK_SIZE+500, NULL, 3, NULL);
+	
+	vDisplayClear();
+	vDisplayWriteStringAtPos(0,0,"PI-Calc FS2022");
+	vTaskStartScheduler();
+	return 0;
+}
 
 
+//////////////////////////////////////////////////////////////////////////
+// Task zur Berechnung von Pi mit float 32 und 64 Bit gleichzeitig
+//////////////////////////////////////////////////////////////////////////
 void vEuler_PI(void* pvParameters)  {
 	uint32_t counter = 1; 
 	float32_t  lokal_euler_pi;
 	float32_t  euler_pi_calc;
-	
-	
+
 	float64_t f_euler_pi_calc;
 	float64_t f_lokal_euler_pi; 
 	
 	
 	for(;;) 
 	{
-		euler_pi_calc = euler_pi_calc + ( 1.0 / pow(counter, 2) );
+		euler_pi_calc = euler_pi_calc + ( 1.0 / pow(counter, 2) );			//float 32Bit
 		lokal_euler_pi = sqrt( (euler_pi_calc * 6.0 ) );
 		g_Euler_PI = lokal_euler_pi; 
-		
-		
-		f_euler_pi_calc = f_add(f_euler_pi_calc, f_div( f_sd(1) , f_pow( f_sd(counter), f_sd(2) ) ));  //zu viel f_sd?
-		f_lokal_euler_pi = f_pow( f_mult( f_euler_pi_calc, f_sd(6)) , f_sd(0.5));  //wurzel mit pow
-		g_f_Euler_PI = f_lokal_euler_pi; 
+
+		f_euler_pi_calc = f_add(f_euler_pi_calc, f_div( f_sd(1) , f_pow( f_sd(counter), f_sd(2) ) ));  //float 64Bit
+		f_lokal_euler_pi = f_pow( f_mult( f_euler_pi_calc, f_sd(6)) , f_sd(0.5));  
 		
 		counter++;
-		//vTaskDelay(1/portTICK_RATE_MS);
 	}
 }
 
-
+//////////////////////////////////////////////////////////////////////////
+// Task zur Berechnung von Pi mit float 32 und 64 Bit gleichzeitig
+//////////////////////////////////////////////////////////////////////////
 void vLeibnizTask(void* pvParameters) {
 	
-	uint32_t counter = 3; 
-	float32_t  lokal_leib_pi;
-	
-	float32_t pi4 = 1;
-	
-	
-	
-	char pi_compare[8];
-	
-	char ululu[] = "12345";
-	char ululu2[] = "13345";
+	uint32_t	counter = 3; 
+	float32_t	lokal_leib_pi;
+	float32_t	pi4 = 1;
+	uint8_t		lokal_task_flag = 0; 
+	char pi_compare[8];						//Überprüfung 5 Kommastellen
 	char pi_comp_loesung[] = "3.14159";
-	
-	uint8_t lokal_task_flag = 0; 
 	
 	while(1)
 	{
-		//task auswahl
 		if ( (xEventGroupGetBits(egPI_Calc) & (TASK_LEIBNITZ | TASK_START_STOP) ) == (TASK_LEIBNITZ | TASK_START_STOP))  //task enable
 		{
 			
@@ -162,32 +162,27 @@ void vLeibnizTask(void* pvParameters) {
 				lokal_task_flag = pdFALSE;
 				
 			}
-			
-			pi4 = pi4 - (1.0 / counter);  //1.0 damit als float erkannt int counter
+			pi4 = pi4 - (1.0 / counter);  //lokale Pi Berechnung
 			counter += 2;
 			pi4 = pi4 + (1. / counter);
 			counter += 2;
 			lokal_leib_pi = pi4 * 4;
 			
-			if ( (xEventGroupGetBits(egPI_Calc) & DATA_READ_REQUEST_LOCK) == DATA_READ_REQUEST_LOCK)
+			if ( (xEventGroupGetBits(egPI_Calc) & DATA_READ_REQUEST_LOCK) == DATA_READ_REQUEST_LOCK)	//Schreibvorgang LCD, globale Variable locked
 			{
 				xEventGroupSetBits(egPI_Calc, DATA_CALCULATION_READY);
 				xEventGroupWaitBits(egPI_Calc, DATA_READ_LOCK_CLEARED, pdTRUE, pdFALSE, portTICK_RATE_MS);
 			}
+			Leib_PI = lokal_leib_pi;	//Lokales Pi in globale Variable speichern
 			
-			Leib_PI = lokal_leib_pi;	
-			
-			sprintf(&pi_compare[0], "%f", lokal_leib_pi);
+			sprintf(&pi_compare[0], "%f", lokal_leib_pi);		//Generieren von lokalem Pi String mit 5 Kommastellen
 			pi_compare[7] = '\0';
-
-			if ( strcmp(pi_comp_loesung, pi_compare ) == 0)
+			if ( strcmp(pi_comp_loesung, pi_compare ) == 0)		//überprüfung 5 Kommastellen
 			{
-				
 				if (lokal_task_flag == pdFALSE )
 				{
 					xEventGroupSetBits(egPI_Calc, TASK_TIME_FINISHED);
 					lokal_task_flag = pdTRUE; 
-					
 				}
 			}
 
@@ -196,10 +191,10 @@ void vLeibnizTask(void* pvParameters) {
 	
 }
 
-
-
-
-
+//////////////////////////////////////////////////////////////////////////
+// Task zur Abfrage der Tasen und Steuerung der verschiedenen Berechnungs-
+// Task
+//////////////////////////////////////////////////////////////////////////
 void vControllerTask(void* pvParameters) {
 	initButtons();
 	uint8_t counter_500ms = 0; 
@@ -217,22 +212,18 @@ void vControllerTask(void* pvParameters) {
 	float32_t lokal_leib_pi;
 	uint8_t mode = STATE_STOP; 
 	bool b_Time_meas_finished;
-	xEventGroupSetBits(egPI_Calc, TASK_LEIBNITZ);		//start with leib
+	xEventGroupSetBits(egPI_Calc, TASK_LEIBNITZ);		//starts with leibnitz task
 	
 	for(;;) {
 		updateButtons();
 		
 		if (counter_500ms == 0)        //ausgabe alle 500 ms, 50 * 10ms
 		{
-			if ((xEventGroupGetBits(egPI_Calc) & BUTTON_START) == BUTTON_START)
+			if ((xEventGroupGetBits(egPI_Calc) & BUTTON_START) == BUTTON_START)			//Hauptmenue, Button Steuerung
 			{
-				
 				mode = STATE_START;
 				xTaskTimeStart = xTaskGetTickCount(); 
 				xEventGroupSetBits(egPI_Calc, TASK_START_STOP);
-				
-
-				
 			}
 			if ((xEventGroupGetBits(egPI_Calc) & BUTTON_STOP) == BUTTON_STOP)
 			{
@@ -244,7 +235,6 @@ void vControllerTask(void* pvParameters) {
 				xEventGroupSetBits(egPI_Calc, RESET_PI);
 				xEventGroupClearBits(egPI_Calc, TASK_TIME_FINISHED);
 				b_Time_meas_finished = pdFALSE; 
-				
 			}
 			if ((xEventGroupGetBits(egPI_Calc) & BUTTON_SWITCH) == BUTTON_SWITCH)
 			{
@@ -254,7 +244,8 @@ void vControllerTask(void* pvParameters) {
 					task_modus = 0; 
 				}
 			}
-			switch (mode)
+			
+			switch (mode)			//Switch für Start und Stop Modus
 			{
 				case STATE_STOP: 
 				
@@ -264,27 +255,21 @@ void vControllerTask(void* pvParameters) {
 					vDisplayWriteStringAtPos(2,0,"begin calculation");
 					vDisplayWriteStringAtPos(3,0,"Start-Stop-Res-Swit");
 					
-					
 				break;	
 				case STATE_START:
 				
 					vDisplayClear();
 					vDisplayWriteStringAtPos(0,0,"Mode:Start, PI: Leib");
 					vDisplayWriteStringAtPos(3,0,"Start-Stop-Res-Swit");
-					
-					
-					
-					xEventGroupSetBits(egPI_Calc, DATA_READ_REQUEST_LOCK);
+					xEventGroupSetBits(egPI_Calc, DATA_READ_REQUEST_LOCK);		//globale Variable auslesen	mit Ressourcenschutz						
 					xEventGroupWaitBits(egPI_Calc, DATA_CALCULATION_READY, pdTRUE, pdFALSE, portMAX_DELAY);
-					//lokale Variable??? mit Gobaler überschreiben??
 					lokal_leib_pi = Leib_PI;
 					xEventGroupSetBits(egPI_Calc, DATA_READ_LOCK_CLEARED);  //berechnung wieder freigeben --> globale variable
 					xEventGroupClearBits(egPI_Calc, DATA_READ_REQUEST_LOCK);
 					sprintf(&s_result_leibn[0], "PI: %.8f", lokal_leib_pi);
 					vDisplayWriteStringAtPos(1,0, "%s", s_result_leibn);
 					
-					
-					if ((xEventGroupGetBits(egPI_Calc) & TASK_TIME_FINISHED) == TASK_TIME_FINISHED)
+					if ((xEventGroupGetBits(egPI_Calc) & TASK_TIME_FINISHED) == TASK_TIME_FINISHED)  //Zeitmessung PI Berechnung und kontinuierliche Ausgabe der Zeit
 					{
 						xTaskTimeStop = xTaskGetTickCount(); 
 						Calculation_Time_ms = (xTaskTimeStop - xTaskTimeStart);
@@ -292,8 +277,6 @@ void vControllerTask(void* pvParameters) {
 						vDisplayWriteStringAtPos(2,0,"time s: %d", Calculation_Time_s);
 						xEventGroupClearBits(egPI_Calc, TASK_TIME_FINISHED);
 						b_Time_meas_finished = pdTRUE;
-						
-						
 					}
 					else
 					{
@@ -306,26 +289,11 @@ void vControllerTask(void* pvParameters) {
 						{
 							vDisplayWriteStringAtPos(2,0,"time s: %d", Calculation_Time_s);
 						}
-						
 					}
-					
-					
-					
-
 				break; 
 				
 			}
-			
-			//euler test
-			//sprintf(&s_result_euler[0], "PI: %.8f", g_Euler_PI);
-			//vDisplayWriteStringAtPos(2,0, "%s", s_result_euler);
-			//
-			////float 64 euler test
-			//char* tempResultString = f_to_string(g_f_Euler_PI, 16, 16);		//Verwandeln einer Double-Variable in einen String
-			//sprintf(s_result_chudnov, "1: %s", tempResultString);			//Einsetzen des Strings in einen anderen String
-			//vDisplayWriteStringAtPos(2,0,"%s", s_result_chudnov);
-
-			xEventGroupClearBits(egPI_Calc, RESET_EG_BUTTONS); //rest buttons
+			xEventGroupClearBits(egPI_Calc, RESET_EG_BUTTONS);  //rest buttons flags
 			counter_500ms = 50; 
 		}
 		else
@@ -361,6 +329,6 @@ void vControllerTask(void* pvParameters) {
 		if(getButtonPress(BUTTON4) == LONG_PRESSED) {
 			
 		}
-		vTaskDelay(10/portTICK_RATE_MS);  //vtast delay until für genauere Zeit?
+		vTaskDelay(10/portTICK_RATE_MS);  
 	}
 }
